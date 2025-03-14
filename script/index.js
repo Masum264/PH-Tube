d3ff80a8cbe82a4f1f113bf3f66e6bd60d0f718c
Masum264 <masumh246@gmail.com>
2025-03-15 02:32:20 +0600

    Added the second section

--- script/index.js
+++ script/index.js
@@ -5,6 +5,18 @@ function loadCategories(){
 }
 
 function displayCategories(categories){
-    console.log(categories);
+    const categoryContainer = document.getElementById("category-container");
+
+    for(let cat of categories){
+
+        console.log(cat);
+        const categorydiv = document.createElement("div");
+
+        categorydiv.innerHTML = `
+          <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+
+        categoryContainer.append(categorydiv);
+    }
 }
 loadCategories();
